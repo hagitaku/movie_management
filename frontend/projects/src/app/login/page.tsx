b44376e2f72362ee43eef1461b4b7e1acd55d0de
hokto <hokuto.k1441@gmail.com>
2024-09-19 19:27:51 +0900

    バリデーションルールの実装を修正

--- frontend/projects/src/app/login/page.tsx
+++ frontend/projects/src/app/login/page.tsx
@@ -13,22 +13,22 @@ export const Login = () =>{
     const loginSchema = yup.object({
         loginId: yup
             .string()
+            .required("必須項目です．")
             .max(16,"16文字以内で入力してください．")
             .test("loginId-validation","アルファベット・数字・記号(_-@$)のみで入力してください．",function (value : any){
                 return !!value.match(/^[0-9a-zA-Z_\-@\$]+$/);
-            })
-            .required("必須項目です．"),
+            }),
         password: yup
             .string()
+            .required("必須項目です．")
             .min(8,"8文字以上32文字以内で入力してください．")
             .max(32,"8文字以上32文字以内で入力してください．")
-            .test("password-validation-available","アルファベット・数字・記号(_-@$)のみで入力してください．", function (value : any){
+            .test("password-validation-available","アルファベット・数字・記号(_-@$)のみで入力してください．", function (value : string){
                 return !!value.match(/^[0-9a-zA-Z_\-@\$]+$/);
             })
-            .test("password-validation-required-character","アルファベットと数字はどちらも1文字以上含めてください．",function (value : any){
+            .test("password-validation-required-character","アルファベットと数字はどちらも1文字以上含めてください．",function (value : string){
                 return !!value.match(/^(?=.*[a-zA-Z])(?=.*[0-9])[_\-@\$a-zA-Z0-9]+$/)
-            })
-            .required("必須項目です．"),
+            }),
     });
     type LoginForm = yup.InferType<typeof loginSchema>;
     const form = useForm<LoginForm>({
