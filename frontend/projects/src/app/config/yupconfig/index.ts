645459d6bce58a2879c620c2e581ddb027dcbfa0
hokto <hokuto.k1441@gmail.com>
2025-03-15 12:39:49 +0900

    自作バリデーションの修正

--- frontend/projects/src/app/config/yupconfig/index.ts
+++ frontend/projects/src/app/config/yupconfig/index.ts
@@ -2,89 +2,67 @@ import * as yup from "yup";
 
 export type MessageParams = {
   path: string;
-  value: any;
-  originalValue: any;
   label: string;
-  type: string;
+  length: number;
 };
 
-const labelText = (prm: MessageParams) => {
-  return prm.label !== "" ? `${prm.label}は` : "";
+const labelText = (label: string) => {
+  return label ? `${label}は` : "";
 };
 
 // エラーメッセージの日本語表記
-const jpConfig = {
+const jpConfig: yup.LocaleObject = {
   mixed: {
-    default: (prm: MessageParams) => `${labelText(prm)}無効です`,
-    required: (prm: MessageParams) => `${labelText(prm)}必須の入力項目です`,
-    oneOf: (prm: MessageParams & { values: any }) =>
-      `${labelText(prm)}次の値のいずれかを入力してください:${prm.values}`,
-    notOneOf: (prm: MessageParams & { values: any }) =>
-      `${labelText(prm)}次の値のいずれかを入力してください:${prm.values}`,
+    default: ({ label }) => `${labelText(label)}無効です`,
+    required: ({ label }) => `${labelText(label)}必須の入力項目です`,
+    oneOf: ({ label, values }) =>
+      `${labelText(label)}次の値のいずれかを入力してください:${values}`,
+    notOneOf: ({ label, values }) =>
+      `${labelText(label)}次の値のいずれかを入力してください:${values}`,
     notType: `形式が違います`,
     defined: ``,
   },
   string: {
-    length: (prm: MessageParams & { length: number }) =>
-      `${labelText(prm)}${prm.length}文字で入力してください`,
-    min: (prm: MessageParams & { min: number }) =>
-      `${labelText(prm)}少なくとも${prm.min}文字で入力してください`,
-    max: (prm: MessageParams & { max: number }) =>
-      `${labelText(prm)}最大${prm.max}文字で入力してください`,
-    matches: (prm: MessageParams & { regex: RegExp }) =>
-      `${labelText(prm)}次の形式と一致する必要があります: "${prm.regex}"`,
-    email: (prm: MessageParams & { regex: RegExp }) =>
-      `${labelText(prm)}メールアドレス形式で入力してください`,
-    onlyAlphabetNumberSymbol: (prm: MessageParams) =>
-      `${labelText(prm)}アルファベット・数字・記号(_-@$)のみで入力してください`,
-    requireAlphabetNumber: (prm: MessageParams) =>
-      `${labelText(prm)}アルファベットと数字はどちらも1文字以上含めてください`,
+    length: ({ label, length }) =>
+      `${labelText(label)}${length}文字で入力してください`,
+    min: ({ label, min }) =>
+      `${labelText(label)}少なくとも${min}文字で入力してください`,
+    max: ({ label, max }) =>
+      `${labelText(label)}最大${max}文字で入力してください`,
+    matches: ({ label, regex }) =>
+      `${labelText(label)}次の形式と一致する必要があります: "${regex}"`,
+    email: ({ label }) =>
+      `${labelText(label)}メールアドレス形式で入力してください`,
   },
 };
 
 yup.setLocale(jpConfig);
-type StringValidationType = {
-  name: string;
-  errorMessage: (prm: MessageParams) => string;
-  isValid: (value: string) => boolean;
-};
 
-const stringValidationList: StringValidationType[] = [
-  {
-    name: "onlyAlphabetNumberSymbol",
-    errorMessage: jpConfig.string.onlyAlphabetNumberSymbol,
-    isValid: (value: string) => {
-      return !!value.match(/^[0-9a-zA-Z_\-@\$]+$/);
-    },
-  },
-  {
-    name: "requireAlphabetNumber",
-    errorMessage: jpConfig.string.requireAlphabetNumber,
-    isValid: (value: string) => {
-      return !!value.match(/^(?=.*[a-zA-Z])(?=.*[0-9])[_\-@\$a-zA-Z0-9]+$/);
-    },
-  },
-];
+yup.addMethod<yup.StringSchema>(
+  yup.string,
+  "onlyAlphabetNumberSymbol",
+  function (message?: string) {
+    return this.matches(new RegExp(/^[0-9a-zA-Z\_\-@$]+$/), {
+      message:
+        message || "アルファベット・数字・記号(_-@$)のみで入力してください",
+    });
+  }
+);
 
-stringValidationList.forEach((validation) => {
-  yup.addMethod<yup.StringSchema>(
-    yup.string,
-    validation.name,
-    function (message: yup.Message = validation.errorMessage) {
-      return this.test(function (value, testContext) {
-        if (value == null || value === "") {
-          return true;
-        }
-        if (validation.isValid(value)) {
-          return true;
-        }
-        return testContext.createError({
-          message,
-        });
-      });
-    }
-  );
-});
+yup.addMethod<yup.StringSchema>(
+  yup.string,
+  "requireAlphabetNumber",
+  function (message?: string) {
+    return this.matches(
+      new RegExp(/^(?=.*[a-zA-Z])(?=.*[0-9])[\_\-@$a-zA-Z0-9]+$/),
+      {
+        message:
+          message || "アルファベットと数字はどちらも1文字以上含めてください",
+      }
+    );
+  }
+);
+
 declare module "yup" {
   interface StringSchema<TType, TContext, TDefault, TFlags> {
     onlyAlphabetNumberSymbol(): StringSchema<TType, TContext, TDefault, TFlags>;
