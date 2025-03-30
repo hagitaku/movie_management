ebc0dbb42459a9ea1395200dfe7fc715a1e8f69a
hokto <hokuto.k1441@gmail.com>
2025-03-30 19:53:49 +0900

    パラメータの定数化

--- frontend/projects/src/app/constants/env/index.ts
+++ frontend/projects/src/app/constants/env/index.ts
@@ -1,2 +1,5 @@
 export const BASE_API_URL = "http://127.0.0.1:8080/";
 export const REGISTER_POST_PATH = "movie/register";
+export const MAX_LOGIN_ID_LENGTH = 16;
+export const MIN_LOGIN_PASSWORD_LENGTH = 8;
+export const MAX_LOGIN_PASSWORD_LENGTH = 32;
--- frontend/projects/src/app/schema/login.ts
+++ frontend/projects/src/app/schema/login.ts
@@ -1,18 +1,23 @@
 import yup from "@/config/yupconfig";
+import {
+  MAX_LOGIN_ID_LENGTH,
+  MAX_LOGIN_PASSWORD_LENGTH,
+  MIN_LOGIN_PASSWORD_LENGTH,
+} from "@/constants";
 
 export const loginSchema = yup.object({
   loginId: yup
     .string()
     .required()
     .label("ログインID")
-    .max(16)
+    .max(MAX_LOGIN_ID_LENGTH)
     .onlyAlphabetNumberSymbol(),
   password: yup
     .string()
     .required()
     .label("パスワード")
-    .min(8)
-    .max(32)
+    .min(MIN_LOGIN_PASSWORD_LENGTH)
+    .max(MAX_LOGIN_PASSWORD_LENGTH)
     .onlyAlphabetNumberSymbol()
     .requireAlphabetNumber(),
 });
