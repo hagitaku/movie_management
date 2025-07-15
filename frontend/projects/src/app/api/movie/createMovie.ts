f24e7a04872f989341e36274bd949ed7fe1fc3dd
hokto <hokuto.k1441@gmail.com>
2025-07-15 12:15:20 +0900

    [Fix] 不要なところを削除
    (このファイル修正は別のprで行います)

--- frontend/projects/src/app/api/movie/createMovie.ts
+++ frontend/projects/src/app/api/movie/createMovie.ts
@@ -6,15 +6,12 @@ type PostCreateMovieRequest = {
   title: string;
   description: string;
   memo: string;
+  // TODO: Set-Cookieで渡すのでいらない
   session_token: string;
 };
 export const postCreate = async (request: PostCreateMovieRequest) => {
   const postUrl = BASE_API_URL + REGISTER_POST_PATH;
   // TODO: エラーハンドリングを追加する
-  //try {
   const response = await axios.post(postUrl, request);
   return response;
-  //} catch (error) {
-  //throw error;
-  //}
 };
