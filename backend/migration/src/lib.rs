1171107e77f093e9c22bcb604553d6846a279adb
hagihara <33248998+hagitaku@users.noreply.github.com>
2026-08-15 14:24:54 +0900

    fix: フォーマットを修正

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -1,8 +1,8 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_accounts;
-mod m20260222000002_create_sessions;
-mod m20260222000003_create_movies;
+mod m20260222_000002_create_sessions;
+mod m20260222_000003_create_movies;
 
 pub struct Migrator;
 
@@ -11,8 +11,8 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_create_accounts::Migration),
-            Box::new(m20260222000002_create_sessions::Migration),
-            Box::new(m20260222000003_create_movies::Migration),
+            Box::new(m20260222_000002_create_sessions::Migration),
+            Box::new(m20260222_000003_create_movies::Migration),
         ]
     }
 }
